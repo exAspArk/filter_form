541b13ed93f217f1e614a39833c73a7a86efd24b
Evgeny Li <exaspark@gmail.com>
2013-12-20 14:35:35 +0400

    add rspec gem

---
+++ spec/models/form_builder_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'FormBuilder' do
+  pending 'comming soon'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'rails'
+require 'action_view'
+require 'active_model'
+
+require 'filter_form'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.tty = true
+  config.order = :random
+end
